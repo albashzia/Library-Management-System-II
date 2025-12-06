6a2a28bee15ab4f33663a37eb3e00a6da1d80fdd
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-12-06 13:48:34 +0000

    Creating Array Lists holding data relevant to books

--- LMS.java
+++ LMS.java
@@ -2,6 +2,12 @@ import java.util.*;
 import java.io.*;
 public class LMS
 {
+  static ArrayList<Integer> bookIDs = new ArrayList<>();
+  static ArrayList<String> titles = new ArrayList<>();
+  static ArrayList<String> authors = new ArrayList<>();
+  static ArrayList<Integer> totalQuantities = new ArrayList<>();
+  static ArrayList<Integer> availableQuantities = new ArrayList<>();
+  static ArrayList<String> bookStatus = new ArrayList<>();
   public static void main(String args[])
   {
     
