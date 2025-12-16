39524d2bfd478e77170d09ce8e32887661cc13ad
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-12-16 16:29:42 +0000

    Declaring other array lists that may be utilized throughout the code

--- LMS.java
+++ LMS.java
@@ -7,7 +7,11 @@ public class LMS
   static ArrayList<String> authors = new ArrayList<>();
   static ArrayList<Integer> totalQuantities = new ArrayList<>();
   static ArrayList<Integer> availableQuantities = new ArrayList<>();
-  
+  static ArrayList<Integer> memberIDs = new ArrayList<>();
+  static ArrayList<Integer> issueBookIDs = new ArrayList<>();
+  static ArrayList<Integer> issueMemberIDs = new ArrayList<>();
+  static ArrayList<String> issueDates = new ArrayList<>();
+  static ArrayList<String> memberNames = new ArrayList<>();
   public static void main(String args[])
   {
     
