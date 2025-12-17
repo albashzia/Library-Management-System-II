1025aef459cfa5159970c62ffd32feb258fe862e
Ahmad Albash Zia <ahmedalbashzia@gmail.com>
2025-12-17 13:52:51 +0000

    Removing final declaration of variables holding file names as final is not important and not needed in this case

--- LMS.java
+++ LMS.java
@@ -12,14 +12,15 @@ public class LMS
   static ArrayList<Integer> issueMemberIDs = new ArrayList<>();
   static ArrayList<String> issueDates = new ArrayList<>();
   static ArrayList<String> memberNames = new ArrayList<>();
-  static final String BOOKSDATAFILE = "books_data.txt";
-  static final String ISSUEDBOOKSDATAFILE = "issued_data.txt";
-  static final String MEMBERSDATAFILE = "members.txt";
-  static final String ROOMSDATAFILE = "rooms.txt";
-  static final int ROOMCOUNT = 5;
+  static String BOOKSDATAFILE = "books_data.txt";
+  static String ISSUEDBOOKSDATAFILE = "issued_data.txt";
+  static String MEMBERSDATAFILE = "members.txt";
+  static String ROOMSDATAFILE = "rooms.txt";
+  static int ROOMCOUNT = 5;
   static int[] roomNumbers = {101, 102, 103, 104, 105};
   static String[] roomStatus = {"Available", "Available", "Available", "Available", "Available"};
   static int[] reservedByMemberID = new int[ROOMCOUNT];
+
   public static void main(String args[])
   {
     
